c234ca6ad7b598982043a05a5e6e9808a06eae17
Phil <pizzapost.mail@gmail.com>
2025-08-02 16:19:28 +0200

    - bossbar - big bug fixes

--- src/main/java/de/pizzapost/all_items/AllItems.java
+++ src/main/java/de/pizzapost/all_items/AllItems.java
@@ -3,50 +3,71 @@ package de.pizzapost.all_items;
 import de.pizzapost.all_items.commands.ModCommands;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.entity.boss.BossBar;
+import net.minecraft.entity.boss.ServerBossBar;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.WorldSavePath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class AllItems implements ModInitializer {
     public static final String MOD_ID = "all_items";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    private static final Path SAVE_PATH = Paths.get("config", "all_items_timer.txt");
 
     static boolean started = false;
     static int sec = 0;
     static int min = 0;
     static int hours = 0;
     static int days = 0;
+    static int collected_items = 0;
+    static List<Item> items = new ArrayList<>();
+    private static Thread timerThread;
 
-    public static void saveTimer() {
+    static List<String> blockedItems = List.of("minecraft:air", "minecraft:barrier", "minecraft:bedrock", "minecraft:chain_command_block", "minecraft:command_block", "minecraft:command_block_minecart", "minecraft:debug_stick", "minecraft:farmland", "minecraft:infested_chiseled_stone_bricks", "minecraft:infested_cobblestone", "minecraft:infested_cracked_stone_bricks", "minecraft:infested_deepslate", "minecraft:infested_mossy_stone_bricks", "minecraft:infested_stone", "minecraft:infested_stone_bricks", "minecraft:jigsaw", "minecraft:knowledge_book", "minecraft:light", "minecraft:mycelium", "minecraft:player_head", "minecraft:podzol", "minecraft:reinforced_deepslate", "minecraft:repeating_command_block", "minecraft:rooted_dirt", "minecraft.spawner", "minecraft:structure_block", "minecraft:structure_void", "minecraft:test_block", "minecraft:test_instance_block", "minecraft:trial_spawner", "minecraft:vault");
+    static ServerBossBar collectedItemsBossbar = new ServerBossBar(Text.translatable("bossbar.all_items.collected_items", collected_items, items.size()), BossBar.Color.GREEN, BossBar.Style.PROGRESS);
+
+    private static Path getSavePath(MinecraftServer server) {
+        return server.getSavePath(WorldSavePath.ROOT).resolve("all_items_data.txt");
+    }
+
+    public static void saveData(MinecraftServer server) {
         try {
-            Files.createDirectories(SAVE_PATH.getParent());
-            String data = started + ";" + days + ";" + hours + ";" + min + ";" + sec;
-            Files.write(SAVE_PATH, data.getBytes());
+            Path path = getSavePath(server);
+            Files.createDirectories(path.getParent());
+            String data = started + ";" + collected_items + ";" + days + ";" + hours + ";" + min + ";" + sec;
+            Files.write(path, data.getBytes());
         } catch (IOException e) {
             LOGGER.error("Failed to save timer state", e);
         }
     }
 
-    public static void loadTimer() {
-        if (!Files.exists(SAVE_PATH)) return;
+    public static void loadData(MinecraftServer server) {
+        Path path = getSavePath(server);
+        if (!Files.exists(path)) return;
 
         try {
-            String[] parts = Files.readString(SAVE_PATH).split(";");
-            if (parts.length == 5) {
+            String[] parts = Files.readString(path).split(";");
+            if (parts.length == 6) {
                 started = Boolean.parseBoolean(parts[0]);
-                days = Integer.parseInt(parts[1]);
-                hours = Integer.parseInt(parts[2]);
-                min = Integer.parseInt(parts[3]);
-                sec = Integer.parseInt(parts[4]);
+                collected_items = Integer.parseInt(parts[1]);
+                days = Integer.parseInt(parts[2]);
+                hours = Integer.parseInt(parts[3]);
+                min = Integer.parseInt(parts[4]);
+                sec = Integer.parseInt(parts[5]);
             }
         } catch (IOException | NumberFormatException e) {
             LOGGER.error("Failed to load timer state", e);
@@ -55,20 +76,44 @@ public class AllItems implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        loadTimer();
         ModCommands.registerModCommands();
         ServerLifecycleEvents.SERVER_STARTED.register(server -> {
-            startGame(server, true);
+            loadData(server);
+            if (started) {
+                startGame(server, true);
+            }
+        });
+        ServerTickEvents.END_WORLD_TICK.register(world -> {
+        });
+        ServerLifecycleEvents.SERVER_STOPPING.register(server -> {
+            started = false;
+            if (timerThread != null && timerThread.isAlive()) {
+                timerThread.interrupt();
+                timerThread = null;
+            }
+            collected_items = 0;
+            sec = min = hours = days = 0;
+            items.clear();
+            collectedItemsBossbar.clearPlayers();
+        });
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            if (started) {
+                collectedItemsBossbar.addPlayer(handler.getPlayer());
+            }
         });
     }
 
     public static void startGame(MinecraftServer server, boolean force) {
-        if (!force) {
-            if (started) return;
-        }
+        if (!force && started) return;
+        if (timerThread != null && timerThread.isAlive()) return;
+
         started = true;
 
-        new Thread(() -> {
+        if (items.isEmpty()) {
+            shuffleItems();
+        }
+
+        timerThread = new Thread(() -> {
             while (started) {
                 try {
                     Thread.sleep(1000);
@@ -78,7 +123,7 @@ public class AllItems implements ModInitializer {
                 }
 
                 if (!server.isPaused()) {
-                    AllItems.sec++;
+                    sec++;
                     if (sec >= 60) {
                         sec = 0;
                         min++;
@@ -91,21 +136,38 @@ public class AllItems implements ModInitializer {
                             }
                         }
                     }
-                    saveTimer();
+                    saveData(server);
                 }
+
+                collectedItemsBossbar.setPercent((float) collected_items / items.size());
+                collectedItemsBossbar.setName(Text.translatable("bossbar.all_items.collected_items", collected_items, items.size()));
+
                 Text actionbarMessage = Text.translatable("actionbar.all_items.timer", days, hours, min, sec).formatted(Formatting.GOLD);
                 for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
                     player.sendMessage(actionbarMessage, true);
+                    collectedItemsBossbar.addPlayer(player);
                 }
             }
-        }).start();
+        });
+
+        timerThread.start();
     }
 
-    public static void setGameTimer(int d, int h, int m, int s) {
+    public static void setGameTimer(MinecraftServer server, int d, int h, int m, int s) {
         days = d;
         hours = h;
         min = m;
         sec = s;
-        saveTimer();
+        saveData(server);
+    }
+
+    public static void shuffleItems() {
+        for (Item item : Registries.ITEM) {
+            if (!blockedItems.contains(item.toString()) && !item.toString().contains("spawn_egg")) {
+                items.add(item);
+            }
+        }
+        System.out.println(items);
+        Collections.shuffle(items);
     }
 }
--- src/main/java/de/pizzapost/all_items/commands/ModCommands.java
+++ src/main/java/de/pizzapost/all_items/commands/ModCommands.java
@@ -3,6 +3,7 @@ package de.pizzapost.all_items.commands;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import de.pizzapost.all_items.AllItems;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
 
 import static net.minecraft.server.command.CommandManager.argument;
@@ -10,22 +11,30 @@ import static net.minecraft.server.command.CommandManager.literal;
 
 public class ModCommands {
     public static void initializeCommands() {
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("start").requires(source -> source.hasPermissionLevel(4)).executes(context -> {
-            ServerCommandSource source = context.getSource();
-            AllItems.startGame(source.getServer(), false);
-            return 1;
-        })));
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(literal("start").requires(source -> source.hasPermissionLevel(2)).executes(context -> {
+                MinecraftServer server = context.getSource().getServer();
+                AllItems.startGame(server, false);
+                return 1;
+            }));
+        });
 
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             dispatcher.register(literal("set_timer").requires(source -> source.hasPermissionLevel(4)).then(argument("days", IntegerArgumentType.integer(0)).then(argument("hours", IntegerArgumentType.integer(0, 23)).then(argument("minutes", IntegerArgumentType.integer(0, 59)).then(argument("seconds", IntegerArgumentType.integer(0, 59)).executes(context -> {
+                ServerCommandSource source = context.getSource();
                 int days = IntegerArgumentType.getInteger(context, "days");
                 int hours = IntegerArgumentType.getInteger(context, "hours");
                 int minutes = IntegerArgumentType.getInteger(context, "minutes");
                 int seconds = IntegerArgumentType.getInteger(context, "seconds");
-                AllItems.setGameTimer(days, hours, minutes, seconds);
+                AllItems.setGameTimer(source.getServer(), days, hours, minutes, seconds);
                 return 1;
             }))))));
         });
+
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("shuffle_items").requires(source -> source.hasPermissionLevel(4)).executes(context -> {
+            AllItems.shuffleItems();
+            return 1;
+        })));
     }
 
     public static void registerModCommands() {
